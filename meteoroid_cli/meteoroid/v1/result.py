8adb1445429b039024b4722cafddc328df594d71
YujiAzama <yuji.azama@gmail.com>
2019-11-14 08:26:34 +0900

    Add result cli

--- meteoroid_cli/meteoroid/v1/result.py
+++ meteoroid_cli/meteoroid/v1/result.py
@@ -1,5 +1,7 @@
 from cliff.show import ShowOne
 from cliff.lister import Lister
+from coreapi import Client
+from coreapi.transports import HTTPTransport
 
 from meteoroid_cli.meteoroid.v1.client.result_client import ResultClient
 from meteoroid_cli.meteoroid.v1.libs.decorator import fiware_arguments
@@ -12,20 +14,19 @@ class ResultShow(ShowOne):
     def get_parser(self, prog_name):
         parser = super().get_parser(prog_name)
         parser.add_argument('id', help='result id')
+        parser.add_argument('--annotations', action='store_true', help='Show annotations')
         return parser
 
     def take_action(self, parsed_args):
-        response = ResultClient().retrieve_result(
+        result = ResultClient().retrieve_result(
             id=parsed_args.id,
             fiware_service=parsed_args.fiwareservice,
             fiware_service_path=parsed_args.fiwareservicepath)
-        columns = []
-        data = []
-        for key, value in response.items():
-            columns.append(key)
-            data.append(value)
-
-        return (tuple(columns), tuple(data))
+        if not parsed_args.annotations:
+            result.pop("annotations")
+        columns = result.keys()
+        data = result.values()
+        return columns, data
 
 
 class ResultList(Lister):
@@ -34,20 +35,18 @@ class ResultList(Lister):
     @fiware_arguments
     def get_parser(self, prog_name):
         parser = super().get_parser(prog_name)
+        parser.add_argument('--annotations', action='store_true', help='Show annotations')
         return parser
 
     def take_action(self, parsed_args):
-        response = ResultClient().list_results(
-            fiware_service=parsed_args.fiwareservice,
-            fiware_service_path=parsed_args.fiwareservicepath
-        )
-        columns = []
-        data = []
-        for index, res in enumerate(response):
-            data.append([])
-            for key, value in res.items():
-                columns.append(key)
-                data[index].append(value)
-        columns = sorted(set(columns), key=columns.index)
-
-        return (tuple(columns), tuple(map(tuple, data)))
+        results = ResultClient().list_results(
+            parsed_args.fiwareservice,
+            parsed_args.fiwareservicepath)
+        if not parsed_args.annotations:
+            for index in range(len(results)):
+                results[index].pop("annotations")
+        if len(results) > 0:
+            columns = results[0].keys()
+            data = [x.values() for x in results]
+            return columns, data
+        return (), ()
