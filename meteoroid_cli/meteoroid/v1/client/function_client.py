3bd63ede646ad8f19696153576279592c845aff2
ryo-ma <saka_ro@yahoo.co.jp>
2019-11-14 18:39:39 +0900

    Support to create function

--- meteoroid_cli/meteoroid/v1/client/function_client.py
+++ meteoroid_cli/meteoroid/v1/client/function_client.py
@@ -7,9 +7,9 @@ class FunctionClient(BaseClient):
         super(FunctionClient, self).__init__()
 
     def list_function(self, fiware_service, fiware_service_path):
-        return self.__action(fiware_service,
-                             fiware_service_path,
-                             ['functions', 'list'])
+        return self._action(fiware_service,
+                            fiware_service_path,
+                            ['functions', 'list'])
 
     def retrieve_function(self, id, fiware_service, fiware_service_path, code=False):
         params = {'id': id}
@@ -20,3 +20,9 @@ class FunctionClient(BaseClient):
                             ['functions', 'read'],
                             params,
                             validate=not code)
+
+    def create_function(self, fiware_service, fiware_service_path, data):
+        return self._action(fiware_service,
+                            fiware_service_path,
+                            ['functions', 'create'],
+                            data)
--- meteoroid_cli/meteoroid/v1/function.py
+++ meteoroid_cli/meteoroid/v1/function.py
@@ -1,8 +1,58 @@
+import sys
+import os
+import base64
+import argparse
 from cliff.show import ShowOne
 from cliff.lister import Lister
 
 from meteoroid_cli.meteoroid.v1.client.function_client import FunctionClient
 from meteoroid_cli.meteoroid.v1.libs.decorator import fiware_arguments
+
+NODE_JS_EXT = '.js'
+PYTHON_EXT = '.py'
+JAVA_EXT = '.jar'
+SWIFT_EXT = '.swift'
+PHP_EXT = '.php'
+RUBY_EXT = '.rb'
+GO_EXT = '.go'
+BAL_BIN_EXT = '.balx'
+ZIP_EXT = '.zip'
+
+NODE_JS = 'nodejs'
+PYTHON = 'python'
+JAVA = 'java'
+SWIFT = 'swift'
+PHP = 'php'
+RUBY = 'ruby'
+GO = 'go'
+
+DEFAULT = 'default'
+
+
+EXT_LANG = {
+    NODE_JS_EXT: NODE_JS,
+    PYTHON_EXT: PYTHON,
+    JAVA_EXT: JAVA,
+    SWIFT_EXT: SWIFT,
+    PHP_EXT: PHP,
+    RUBY_EXT: RUBY,
+    GO_EXT: GO
+}
+
+
+class StoreKeyPair(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if namespace.param is None:
+            param = []
+        else:
+            param = namespace.param
+        if len(values) == 2:
+            k, v = values
+            param.append({
+                'key': k,
+                'value': v
+            })
+        setattr(namespace, self.dest, param)
 
 
 class FunctionShow(ShowOne):
@@ -12,7 +62,7 @@ class FunctionShow(ShowOne):
     def get_parser(self, prog_name):
         parser = super().get_parser(prog_name)
         parser.add_argument('id', help='function id')
-        parser.add_argument('--code', action='store_true', help='Show the source code')
+        parser.add_argument('-c', '--code', action='store_true', help='Show the source code')
         return parser
 
     def take_action(self, parsed_args):
@@ -45,3 +95,52 @@ class FunctionList(Lister):
             data = [x.values() for x in response]
             return columns, data
         return (), ()
+
+
+class FunctionCreate(ShowOne):
+    "Create a function"
+
+    @fiware_arguments
+    def get_parser(self, prog_name):
+        parser = super().get_parser(prog_name)
+        parser.add_argument('name', help='Function name')
+        parser.add_argument('file',
+                            type=argparse.FileType('rb'),
+                            help='Function file name')
+        parser.add_argument('-l', '--language', help='Program language')
+        parser.add_argument('-p', '--param',
+                            nargs=2,
+                            action=StoreKeyPair,
+                            help='Inject param to Function (--param KEY VALUE)')
+        return parser
+
+    def take_action(self, parsed_args):
+        data = {
+            'name': parsed_args.name
+        }
+        _, extension = os.path.splitext(parsed_args.file.name)
+        if extension == ZIP_EXT or extension == JAVA_EXT or extension == BAL_BIN_EXT:
+            data['code'] = base64.b64encode(parsed_args.file.read())
+            data['binary'] = True
+        else:
+            data['code'] = parsed_args.file.read()
+
+        if parsed_args.language is not None:
+            data['language'] = parsed_args.language
+        else:
+            if extension != ZIP_EXT:
+                data['language'] = self.__get_default_language(extension)
+        if parsed_args.param is not None:
+            data['parameters'] = parsed_args.param
+        response = FunctionClient().create_function(
+            fiware_service=parsed_args.fiwareservice,
+            fiware_service_path=parsed_args.fiwareservicepath,
+            data=data
+        )
+        columns = response.keys()
+        data = response.values()
+        return columns, data
+
+    def __get_default_language(self, extension):
+        language = EXT_LANG[extension]
+        return f'{language}:{DEFAULT}'
